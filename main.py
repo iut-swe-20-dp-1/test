a7db7f7a10fe2aeaaef181ac756b4aefe4a1093b
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-09-28 10:30:11 +0600

    fun and test fun

---
+++ main.py
@@ -0,0 +1,4 @@
+def add_value_to_stack(stack_list):
+    if len(stack_list) > 10:
+        raise ValueError("A maximum of 10 values can be added")
+    return {"stack": stack_list}
---
+++ test_sample.py
@@ -0,0 +1,11 @@
+import unittest
+from main import add_value_to_stack
+
+class TestStack(unittest.TestCase):
+    def test_add_value_to_stack(self):
+        actual = add_value_to_stack(["1", "2", "3"])
+        expected = {"stack": ["1", "3"]}
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
